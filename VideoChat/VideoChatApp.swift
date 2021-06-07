35b28571f0e728ae6c94ec36977b9729f64620cb
benzoms <31530092+benzoms@users.noreply.github.com>
2021-06-06 18:31:43 -0700

    Add files via upload

---
+++ VideoChat/VideoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoChatApp.swift
+//  VideoChat
+//
+//  Created by Ben Zhang on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
